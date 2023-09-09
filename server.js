b1bd1703970d057753753f955577123cc59b0722
GH-TIGON <110551056+GH-TIGON@users.noreply.github.com>
2023-09-09 22:35:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
-// 기존 코드
 const express = require('express');
-const axios = require('axios');
+const request = require('request');
 const app = express();
-const port = 3000;
 
-app.use(express.json());
+app.use(express.static('public'));  // 'public' 디렉토리의 정적 파일을 제공
 
-// GET 요청에 대한 핸들러
-app.get('/api/hello', (req, res) => {
-  res.send('Hello, World!');
+app.get('/proxy', function(req, res) {
+    const urlToProxy = req.query.url;
+
+    // 요청된 URL로 요청을 전달하고 응답을 클라이언트에게 돌려줍니다.
+    request({url: urlToProxy}).pipe(res);
 });
 
-// POST 요청에 대한 핸들러
-app.post('/api/data', (req, res) => {
-  const data = req.body;
-  // 데이터 처리 로직...
-  res.send('Data received successfully!');
-});
-
-app.get('/proxy', async (req, res) => {
-  const urlToProxy = req.query.url;
-
-  try {
-    const response = await axios.get(urlToProxy);
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send({ error: 'Error occurred while trying to proxy' });
-  }
-});
-
-// 여기부터 추가된 코드
-app.use(express.static('public')); // public 폴더 안에 있는 정적 파일들을 호스팅합니다.
-
-// 기존 코드
-app.listen(port, () => {
-  console.log(`Proxy server listening at http://localhost:${port}`);
+app.listen(3000, function() {
+    console.log('Proxy server is running on port 3000')
 });
